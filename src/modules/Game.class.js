d639b6ad67a204bb1b9d833b498d9149ea2750bc
Bohdan <takhaiev@icloud.com>
2025-06-26 23:25:39 +0300

    done task

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
   /**
    * Creates a new game instance.
@@ -13,56 +8,319 @@ class Game {
    * The initial state of the board.
    * @default
    * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
+   * [0, 0, 0, 0],
+   * [0, 0, 0, 0],
+   * [0, 0, 0, 0]]
    *
    * If passed, the board will be initialized with the provided
    * initial state.
    */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    if (initialState) {
+      this.board = initialState;
+    } else {
+      this.board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
+    }
+
+    this.score = 0;
+    this.status = 'idle';
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    let boardChanged = false;
+    let moveScore = 0;
+
+    for (let r = 0; r < 4; r++) {
+      const row = this.board[r];
+      const { newLine, score, change } = this.slideAndMerge(row);
+
+      this.board[r] = newLine;
+      moveScore += score;
+
+      if (change) {
+        boardChanged = true;
+      }
+    }
+
+    if (boardChanged) {
+      this.score += moveScore;
+      this.addNewTile();
+      this.checkWin();
+    }
+
+    this.checkLose();
+  }
+
+  moveRight() {
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    let boardChanged = false;
+    let moveScore = 0;
+
+    for (let r = 0; r < 4; r++) {
+      const row = [...this.board[r]].reverse();
+      const { newLine, score, change } = this.slideAndMerge(row);
+
+      this.board[r] = [...newLine].reverse();
+      moveScore += score;
+
+      if (change) {
+        boardChanged = true;
+      }
+    }
+
+    if (boardChanged) {
+      this.score += moveScore;
+      this.addNewTile();
+      this.checkWin();
+    }
+
+    this.checkLose();
+  }
+
+  moveUp() {
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    let boardChanged = false;
+    let moveScore = 0;
+
+    for (let c = 0; c < 4; c++) {
+      const collumn = [];
+
+      for (let row = 0; row < 4; row++) {
+        collumn.push(this.board[row][c]);
+      }
+
+      const { newLine, score, change } = this.slideAndMerge(collumn);
+
+      for (let r = 0; r < 4; r++) {
+        this.board[r][c] = newLine[r];
+      }
+      moveScore += score;
+
+      if (change) {
+        boardChanged = true;
+      }
+    }
+
+    if (boardChanged) {
+      this.score += moveScore;
+      this.addNewTile();
+      this.checkWin();
+    }
+
+    this.checkLose();
+  }
+
+  moveDown() {
+    if (this.status !== 'playing') {
+      return;
+    }
+
+    let boardChanged = false;
+    let moveScore = 0;
+
+    for (let c = 0; c < 4; c++) {
+      const column = [];
+
+      for (let r = 0; r < 4; r++) {
+        column.push(this.board[r][c]);
+      }
+
+      const revColumn = [...column].reverse();
+      const { newLine, score, change } = this.slideAndMerge(revColumn);
+
+      const finalColumn = [...newLine].reverse();
+
+      for (let r = 0; r < 4; r++) {
+        this.board[r][c] = finalColumn[r];
+      }
+
+      moveScore += score;
+
+      if (change) {
+        boardChanged = true;
+      }
+    }
+
+    if (boardChanged) {
+      this.score += moveScore;
+      this.addNewTile();
+      this.checkWin();
+    }
+
+    this.checkLose();
+  }
 
   /**
-   * @returns {number}
-   */
-  getScore() {}
-
-  /**
-   * @returns {number[][]}
-   */
-  getState() {}
-
-  /**
-   * Returns the current game status.
-   *
-   * @returns {string} One of: 'idle', 'playing', 'win', 'lose'
-   *
    * `idle` - the game has not started yet (the initial state);
    * `playing` - the game is in progress;
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+  start() {
+    if (
+      this.status === 'idle' ||
+      this.status === 'win' ||
+      this.status === 'lose' ||
+      this.status === 'playing'
+    ) {
+      this.board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ];
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+      this.score = 0;
+      this.status = 'playing';
+      this.addNewTile();
+      this.addNewTile();
+    }
+  }
 
-  // Add your own methods here
+  addNewTile() {
+    const emptyCell = [];
+
+    for (let row = 0; row < 4; row++) {
+      for (let cell = 0; cell < 4; cell++) {
+        if (this.board[row][cell] === 0) {
+          emptyCell.push({ row, cell });
+        }
+      }
+    }
+
+    if (emptyCell.length > 0) {
+      const randomNumber = Math.floor(Math.random() * emptyCell.length);
+      const { row, cell } = emptyCell[randomNumber];
+
+      this.board[row][cell] = Math.random() < 0.1 ? 4 : 2;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  slideAndMerge(line) {
+    let score = 0;
+    let change = false;
+    const initialLine = [...line];
+
+    const filteredLine = line.filter((cell) => cell !== 0);
+    const newLine = [];
+    const mergedThisMove = new Array(4).fill(false);
+
+    for (let i = 0; i < filteredLine.length; i++) {
+      if (
+        i + 1 < filteredLine.length &&
+        filteredLine[i] === filteredLine[i + 1] &&
+        !mergedThisMove[i] &&
+        !mergedThisMove[i + 1]
+      ) {
+        newLine.push(filteredLine[i] * 2);
+
+        score += filteredLine[i] * 2;
+
+        mergedThisMove[i + 1] = true;
+        i++;
+      } else {
+        newLine.push(filteredLine[i]);
+      }
+    }
+
+    while (newLine.length < 4) {
+      newLine.push(0);
+    }
+
+    if (JSON.stringify(initialLine) !== JSON.stringify(newLine)) {
+      change = true;
+    }
+
+    return {
+      newLine,
+      score,
+      change,
+    };
+  }
+
+  restart() {
+    this.start();
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.board;
+  }
+
+  hasAbilityMove() {
+    for (let row = 0; row < 4; row++) {
+      for (let cell = 0; cell < 4; cell++) {
+        if (this.board[row][cell] === 0) {
+          return true;
+        }
+      }
+    }
+
+    for (let row = 0; row < 4; row++) {
+      for (let cell = 0; cell < 4; cell++) {
+        if (cell < 3 && this.board[row][cell] === this.board[row][cell + 1]) {
+          return true;
+        }
+
+        if (row < 3 && this.board[row][cell] === this.board[row + 1][cell]) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  checkWin() {
+    for (let row = 0; row < 4; row++) {
+      for (let cell = 0; cell < 4; cell++) {
+        if (this.board[row][cell] === 2048) {
+          this.status = 'win';
+
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  checkLose() {
+    if (!this.hasAbilityMove()) {
+      this.status = 'lose';
+
+      return true;
+    }
+
+    return false;
+  }
 }
 
-module.exports = Game;
+export default Game;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,106 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+import Game from '../modules/Game.class.js';
 
-// Write your code here
+const game = new Game();
+const start = document.querySelector('.start');
+
+start.addEventListener('click', () => {
+  if (start.textContent === 'Start') {
+    start.classList.add('restart');
+    start.classList.remove('start');
+    start.textContent = 'Restart';
+    game.start();
+    updateField();
+    // updateStatus();
+  }
+
+  if (start.textContent === 'Restart') {
+    game.start();
+    updateField();
+    // updateStatus();
+  }
+});
+
+function updateStatus() {
+  const messageStart = document.querySelector('.message-start');
+  const messageLose = document.querySelector('.message-lose');
+  const messageWin = document.querySelector('.message-win');
+  const gameStatus = game.getStatus();
+
+  messageLose.classList.add('hidden');
+  messageStart.classList.add('hidden');
+  messageWin.classList.add('hidden');
+
+  if (gameStatus === 'playing') {
+    messageStart.classList.add('hidden');
+  } else if (gameStatus === 'lose') {
+    messageLose.classList.remove('hidden');
+  } else if (gameStatus === 'win') {
+    messageWin.classList.remove('hidden');
+  }
+}
+
+function move() {
+  const score = document.querySelector('.game-score');
+
+  updateField();
+  updateStatus();
+  score.textContent = game.getScore();
+}
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowUp':
+      event.preventDefault();
+      game.moveUp();
+      move();
+      break;
+    case 'ArrowDown':
+      e.preventDefault();
+      game.moveDown();
+      move();
+      break;
+    case 'ArrowLeft':
+      e.preventDefault();
+      game.moveLeft();
+      move();
+      break;
+    case 'ArrowRight':
+      e.preventDefault();
+      game.moveRight();
+      move();
+      break;
+  }
+});
+
+function updateField() {
+  const gameBoard = game.getState();
+  const fieldCells = document.querySelectorAll('.field-cell');
+
+  fieldCells.forEach((cellElement) => {
+    Array.from(cellElement.classList).forEach((cls) => {
+      if (cls.startsWith('field-cell--')) {
+        cellElement.classList.remove(cls);
+      }
+    });
+  });
+
+  let cellIndex = 0;
+
+  for (let r = 0; r < 4; r++) {
+    for (let c = 0; c < 4; c++) {
+      const cellValue = gameBoard[r][c];
+      const cellElement = fieldCells[cellIndex];
+
+      if (cellValue !== 0) {
+        cellElement.textContent = cellValue;
+        cellElement.classList.add(`field-cell--${cellValue}`);
+      } else {
+        cellElement.textContent = '';
+      }
+
+      cellIndex++;
+    }
+  }
+}
